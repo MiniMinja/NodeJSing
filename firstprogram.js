044ab3f205b9b91bc35fc7fb4667352e69351d99
Minyoung Heo <minyoungheo.heo@gmail.com>
2018-07-06 23:32:19 -0700

    changed first programm to match the book im reading

--- firstprogram.js
+++ firstprogram.js
@@ -5,6 +5,8 @@ var http = require('http');
 //creating a server application
 //this is called whenever a request is made to the application
 http.createServer(function(req, res){
-  res.writeHead(200,{'Content-Type':'text/html'});
-  res.end('Hello World');
-}).listen(8080);
+  res.writeHead(200,{'content-type':'text/plain'});
+  res.end('Hello World\n');
+}).listen(8124);
+
+console.log('Server running on 8124');
